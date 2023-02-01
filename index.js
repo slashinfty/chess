bba092df75cc6e2faff23dfb8644a96f9818d4b8
Matt Braddock <matt.braddock@gmail.com>
2023-01-31 21:47:52 -0500

    initial setup

---
+++ index.js
@@ -0,0 +1,9 @@
+/*function exportButton() {
+    const element = document.createElement('a');
+    element.setAttribute('href', `data:text/plain;charset=utf-8,${encodeURIComponent(JSON.stringify(tournament, null, 4))}`);
+    element.setAttribute('download', `${tournament.name}.json`);
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+}*/
